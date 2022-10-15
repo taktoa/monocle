103ded0b5bd8adc7d4eec9bab3e198a92e8a03d1
Remy Goldschmidt <taktoa@gmail.com>
2022-10-15 13:35:08 -0700

    shadertoy style vertex and frag shader

--- monocle/src/tracking.rs
+++ monocle/src/tracking.rs
@@ -535,7 +535,7 @@ impl VulkanEngine {
         let vertex_shader_source = r#"
           #version 450
 
-          layout(location = 0) out vec3 fragColor;
+          layout(location = 0) out vec2 position;
 
           vec2 positions[6] = vec2[](
               vec2(-1.0, 1.0),
@@ -546,29 +546,22 @@ impl VulkanEngine {
               vec2(1.0, 1.0)
           );
 
-          vec3 colors[6] = vec3[](
-              vec3(1.0, 0.0, 0.0),
-              vec3(0.0, 1.0, 0.0),
-              vec3(0.0, 0.0, 1.0),
-              vec3(1.0, 0.0, 0.0),
-              vec3(0.0, 1.0, 0.0),
-              vec3(0.0, 0.0, 1.0)
-          );
-
           void main() {
-              gl_Position = vec4(positions[gl_VertexIndex], 0.0, 1.0);
-              fragColor = colors[gl_VertexIndex];
+            vec2 pos = positions[gl_VertexIndex];
+            position = (pos + 1.0) / 2.0;
+            gl_Position = vec4(-pos.x, -pos.y, 0.0, 1.0);
           }
         "#;
 
         let fragment_shader_source = r#"
           #version 450
 
-          layout(location = 0) in vec3 fragColor;
-          layout(location = 0) out vec4 outColor;
+          layout(location = 0) in vec2 position;
+          layout(location = 0) out vec4 color;
+          layout(binding = 0) uniform sampler2D image;
 
           void main() {
-              outColor = vec4(fragColor, 1.0);
+              color = texture(image, position);
           }
         "#;
 
