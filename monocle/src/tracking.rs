dcac9c47bc87f429526f1a36ce5d628728d60c03
Remy Goldschmidt <taktoa@gmail.com>
2022-10-15 01:41:09 -0700

    switch back to working shaders

--- monocle/src/tracking.rs
+++ monocle/src/tracking.rs
@@ -489,52 +489,52 @@ impl VulkanEngine {
         let vertex_shader_source = r#"
           #version 450
 
-          layout(location = 0) in vec2 pos;
-          layout(location = 0) out vec2 v_position;
-
-          void main() {
-            v_position = (pos + 1.0) / 2.0;
-            gl_Position = vec4(-pos.x, -pos.y, 0.0, 1.0);
-          }
-
-          // layout(location = 0) out vec3 fragColor;
-          //
-          // vec2 positions[3] = vec2[](
-          //     vec2(0.0, -0.5),
-          //     vec2(0.5, 0.5),
-          //     vec2(-0.5, 0.5)
-          // );
-          //
-          // vec3 colors[3] = vec3[](
-          //     vec3(1.0, 0.0, 0.0),
-          //     vec3(0.0, 1.0, 0.0),
-          //     vec3(0.0, 0.0, 1.0)
-          // );
+          // layout(location = 0) in vec2 pos;
+          // layout(location = 0) out vec2 v_position;
           //
           // void main() {
-          //     gl_Position = vec4(positions[gl_VertexIndex], 0.0, 1.0);
-          //     fragColor = colors[gl_VertexIndex];
+          //   v_position = (pos + 1.0) / 2.0;
+          //   gl_Position = vec4(-pos.x, -pos.y, 0.0, 1.0);
           // }
+
+          layout(location = 0) out vec3 fragColor;
+
+          vec2 positions[3] = vec2[](
+              vec2(0.0, -0.5),
+              vec2(0.5, 0.5),
+              vec2(-0.5, 0.5)
+          );
+
+          vec3 colors[3] = vec3[](
+              vec3(1.0, 0.0, 0.0),
+              vec3(0.0, 1.0, 0.0),
+              vec3(0.0, 0.0, 1.0)
+          );
+
+          void main() {
+              gl_Position = vec4(positions[gl_VertexIndex], 0.0, 1.0);
+              fragColor = colors[gl_VertexIndex];
+          }
         "#;
 
         let fragment_shader_source = r#"
           #version 450
 
-          layout(location = 0) in vec2 v_position;
-          layout(location = 0) out vec4 color;
-          layout(binding = 0) uniform sampler2D u_image;
-
-          void main() {
-            vec2 pos = v_position;
-            color = texture(u_image, pos);
-          }
-
-          // layout(location = 0) in vec3 fragColor;
-          // layout(location = 0) out vec4 outColor;
+          // layout(location = 0) in vec2 v_position;
+          // layout(location = 0) out vec4 color;
+          // layout(binding = 0) uniform sampler2D u_image;
           //
           // void main() {
-          //     outColor = vec4(fragColor, 1.0);
+          //   vec2 pos = v_position;
+          //   color = texture(u_image, pos);
           // }
+
+          layout(location = 0) in vec3 fragColor;
+          layout(location = 0) out vec4 outColor;
+
+          void main() {
+              outColor = vec4(fragColor, 1.0);
+          }
         "#;
 
         let compiler = shaderc::Compiler::new().unwrap();
